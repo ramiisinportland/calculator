681ddfcaf398f8633d9b4a1edddf7be98cadb371
Erik Barron and Rami Ahmed <rami@>
2020-01-13 13:23:36 -0800

    Add BMI calculator, comment out add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 
 
-var add = function(number1, number2){
-    return number1 + number2;
-};
+// var add = function(number1, number2){
+//     return number1 + number2;
+// };
 
-var number1 = parseInt(prompt("Enter a number"));
+// var number1 = parseInt(prompt("Enter a number: "));
 
-var number2 = parseInt(prompt("Enter another number"));
+// var number2 = parseInt(prompt("Enter another number: "));
 
-var result = add(number1, number2);
+// var result = add(number1, number2);
 
-alert(result);
+// alert(result);
+
+
+var height = parseInt(prompt("Enter your heights in inches: "));
+var weight = parseInt(prompt("Enter your weight with pounds: "));
+var BMI    = ( weight / (height * height)) * 703;
+
+alert(BMI);
