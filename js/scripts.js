4ac098813f839ffeb266a384e99d68e5f4f1ad10
Rami Ahmed and Stephanie Podolak <rami@>
2020-01-15 15:34:47 -0800

    develop user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
+ var add = function(number1, number2){
+	return number1 + number2;
+ };
+ 
+var subtract = function(number1, number2) {
+	return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+	return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+	return number1 / number2;
+};
 
 
-// var add = function(number1, number2){
-//     return number1 + number2;
-// };
-
-// var number1 = parseInt(prompt("Enter a number: "));
-
-// var number2 = parseInt(prompt("Enter another number: "));
-
-// var result = add(number1, number2);
-
-// alert(result);
 
 
-var height = parseInt(prompt("Enter your heights in inches: "));
-var weight = parseInt(prompt("Enter your weight with pounds: "));
-var BMI    = ( weight / (height * height)) * 703;
 
-alert(BMI);
+$(document).ready(function()    {
+	$("form#add").submit(function()	{
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+});
